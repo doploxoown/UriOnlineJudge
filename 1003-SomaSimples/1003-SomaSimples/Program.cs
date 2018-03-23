dc3d658caea3b688845c4ee7850dc5ad52d0328f
Thiago Figueira <thi.figueira@hotmail.com>
2018-03-22 22:55:06 -0300

    1003 - Soma Simples
    Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.
    
    Entrada
    O arquivo de entrada contém 2 valores inteiros.
    
    Saída
    Imprima a variável SOMA com todas as letras maiúsculas, com um espaço em branco antes e depois da igualdade seguido pelo valor correspondente à soma de A e B. Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".

---
+++ 1003-SomaSimples/1003-SomaSimples/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1003_SomaSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, SOMA;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            SOMA = a + b;
+            Console.WriteLine("SOMA = " + SOMA);
+            Console.ReadKey();
+        }
+    }
+}
