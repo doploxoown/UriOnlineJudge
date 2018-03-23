a23363abe8bed643cc2b326f5d583071034c431d
Thiago Figueira <thi.figueira@hotmail.com>
2018-03-22 23:02:51 -0300

    1004-Produto Simples
    Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.
    
    Entrada
    O arquivo de entrada contém 2 valores inteiros.
    
    Saída
    Imprima a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.

---
+++ 1004-Produto Simples/1004-Produto Simples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1004_Produto_Simples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, PROD;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            PROD = a * b;
+            Console.WriteLine("PROD = " + PROD);
+        }
+    }
+}
