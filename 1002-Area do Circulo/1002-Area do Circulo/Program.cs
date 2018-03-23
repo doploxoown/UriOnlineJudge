da49e91ab8dcd2a452cc21703cfddb7e43e7e3a6
Thiago Figueira <thi.figueira@hotmail.com>
2018-03-22 22:42:09 -0300

    1001-Extremamente Básico
    A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
    
    - Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
    
    Entrada
    A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
    
    Saída
    Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double). Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".

---
+++ 1002-Area do Circulo/1002-Area do Circulo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1002_Area_do_Circulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159, r;
+            r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("A=" + (pi * r * r).ToString("0.0000"));
+            Console.ReadKey();
+            
+        }
+    }
+}
